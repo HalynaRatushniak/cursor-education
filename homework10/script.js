6896559f480da33fc602f6e022daaf5734a3d352
HalynaRatushniak <galinka.ratushnyak@gmail.com>
2023-01-20 18:25:26 +0200

    added hw 10

---
+++ homework10/script.js
@@ -0,0 +1,18 @@
+
+function playSound(e) {
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const keyButton = document.querySelector(`.key[data-key="${e.keyCode}"]`).classList.add('playing');
+
+    if (!sound) return;
+    sound.currentTime = 0;
+    sound.play();
+    keyButton.classList.add('playing');
+};
+
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+document.querySelectorAll('.key').forEach(keyButton => keyButton.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
